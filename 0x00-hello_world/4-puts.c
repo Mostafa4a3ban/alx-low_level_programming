5dea87805702a1f3e0e59f5baa9c0e00cebef8ea
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-16 22:33:49 +0000

    trailing whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
+
 /**
  *main function
- *put
- *return always 0	
+ *
+ *return always
  */
-
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
