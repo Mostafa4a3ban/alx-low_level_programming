3577f2aed8164ff0489df27ef3ee76083403b2d1
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-16 23:20:01 +0000

    sdsd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,10 @@
 int main(void)
 {
 int a;
-char b;
 unsigned int u;
 float f;
-printf("Size of a long long int: %d byte(s)\n", (int)sizeof(a));
-printf("Size of a long long int: %c byte(s)\n", (char)sizeof(b));
-printf("Size of a long long int: %u byte(s)\n", (unsigned int)sizeof(u));
-printf("Size of a long long int: %f byte(s)\n", (float)sizeof(f));
+printf("Size of a  int: %d byte(s)\n", (int)sizeof(a));
+printf("Size of a unsigned int: %u byte(s)\n", (unsigned int)sizeof(u));
+printf("Size of a float int: %f byte(s)\n", (float)sizeof(f));
 return (0);
 }
