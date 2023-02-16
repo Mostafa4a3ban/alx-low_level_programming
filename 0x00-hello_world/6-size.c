93ba03b2a7d035b86a1818f3fe690209aca6d613
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-16 23:13:20 +0000

    sd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,10 @@ int main(void)
 int a;
 char b;
 unsigned int u;
-long long r;
 float f;
 printf("%d", (int)sizeof(a));
 printf("%c", (char)sizeof(b));
 printf("%u", (unsigned int)sizeof(u));
-printf("%ld", (long unsigned int)sizeof(r));
 printf("%f", (float)sizeof(f));
 return (0);
 }
