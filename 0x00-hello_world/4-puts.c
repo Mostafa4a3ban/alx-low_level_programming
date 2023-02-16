1036ee46a8915a17c0d479e0865d6ca155481ef1
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-16 22:28:09 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/*
+/**
  *main function
  *put
- *
+ *return always 0	
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle);
-return 0;
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
 }
