7d17ceaab34be61b037b64d3eec948a11a4d43b4
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-20 23:19:23 +0000

    sd

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,10 +9,10 @@ int main(void)
 int n;
 scanf("%d", &n);
 if (n > 0)
-	printf("%d is positive", n);
+	printf("%i is positive", n);
 else if (n == 0)
-	printf("%d is zero", n);
+	printf("%i is zero", n);
 else
-	printf("%d is negative", n);
+	printf("%i is negative", n);
 return (0);
 }
