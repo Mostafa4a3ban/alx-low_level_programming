23d77f41e0d3cddbfc3eea7bd9088c5eaf9257de
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-28 00:20:50 +0000

    absloutly

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,16 @@
 **/
 void print_alphabet(void)
 {
-int i;
+int i, count = 0;
 for (i = 97; i <= 122; i++)
 {
 	_putchar(i);
+	if (i == 122 && count < 10)
+	{
+	i = 97;
+	count++;
+	_putchar('\n');
+	}
 }
 	_putchar('\n');
 
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print the alphabet from a to z in lowecase 10 times
+*
+*
+**/
+void print_alphabet_x10(void)
+{
+int i, count = 0;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+if (i == 122 && count < 9)
+{
+i = 96;
+count++;
+_putchar('\n');
+}
+}
+_putchar('\n');
+
+}
