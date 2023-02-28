090b1dd68fec79ad7e02861ffc0580ecfa1a5436
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-28 00:41:52 +0000

    qwqwq

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
 void print_alphabet_x10(void)
 {
 int i, count = 0;
-for (i = 97; i <= 122; i++)
+for (i = 97; i <= 122; )
 {
 _putchar(i);
 if (i == 122 && count < 9)
@@ -16,6 +16,7 @@ i = 96;
 count++;
 _putchar('\n');
 }
+i++;
 }
 _putchar('\n');
 }
