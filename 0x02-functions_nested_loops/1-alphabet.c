b08ca7dcface3902d76354dd935effa7a4ebd3ee
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-02-28 00:02:05 +0000

    well done twice

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
-  *
-  *
-  *
-  **/
+ * print_alphabet - print the alphabet from a to z in lowecase
+ *
+ *
+**/
 void print_alphabet(void)
 {
 int i;
-for (i = 97;i <= 122; i++)
+for (i = 97; i <= 122; i++)
 	_putchar(i);
 }
-
