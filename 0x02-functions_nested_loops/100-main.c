b5fcafff7d24f8815622dc15576f3eb1b8b9de15
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-03-07 12:49:16 +0000

    great job

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *mian-the main function
+  *Return: always returrs zero
+  **/
+int main(void)
+{
+print_times_table(3);
+printf("\n");
+print_times_table(5);
+printf("\n");
+print_times_table(98);
+printf("\n");
+print_times_table(12);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_times_table - print time table
+  *@n: the number
+  *
+  **/
+void print_times_table(int n)
+{
+int i, j;
+if (n >= 0 && n <= 15)
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			printf("%d", i*j);
+			if(j != n)
+				printf(", ");
+			else
+				printf("\n");
+		}
+			printf("\n");
+	}
+}
+
