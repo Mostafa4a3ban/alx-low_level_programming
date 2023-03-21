bc63875ac26310f33db7cd0a14a51df7c22e93cc
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-03-21 18:56:38 +0000

    right

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *print_diagonal - to print it diagonal
+  *@n: to count diagonals
+  *
+  **/
+void print_diagonal(int n)
+{
+	int i = 0, count = 0;
+	if(n > 0)
+	{
+	for( i =1; i <= n; i++)
+	{
+		for(count = 1; count < i; count++)
+		{
+			_putchar(' ');
+		}
+		_putchar('/');
+		_putchar('\n');
+	}
+	}
+	else
+		_putchar('\n');
+}
