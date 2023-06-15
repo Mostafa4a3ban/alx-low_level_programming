88886f7565467c9bd888a237df96e05dee33d120
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-15 03:46:01 +0300

    i made argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- the main function
+ * @argc: the arguments count
+ * @argv: the arguments vector
+ *
+ * Return: Always return 0 after finishing*
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
