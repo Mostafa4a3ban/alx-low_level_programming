41534366d74844cca8a52f51fe53ededcb1e0d87
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-15 03:30:16 +0300

    argc, argV is ok with me

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * main - the main function
+ * @argc: the arguments count
+ *@argv: the arguments vector
+ *
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n",argv[argc-1]);
+	return (0);
+}
