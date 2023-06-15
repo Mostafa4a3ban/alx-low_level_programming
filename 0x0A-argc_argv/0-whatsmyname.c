378f0dbf9b091188800d721678102435d6c21098
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-15 03:36:20 +0300

    i edited the betty code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
- *
- * main - the main function
+ * main- the main function
  * @argc: the arguments count
- *@argv: the arguments vector
+ * @argv: the arguments vector
  *
+ * Return: Always return 0 after finishing
  */
+
 int main(int argc, char *argv[])
 {
-	printf("%s\n",argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
