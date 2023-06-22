4297808aec2fedb5731ed6d05f611bd934a7f568
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-22 04:59:43 +0300

    concatinartion

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *str_concat - concatinate.
+ * @s1: pointer to strring
+ * @s2: pointer to string 2.
+ *
+ * Return: Returns NULL or pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	unsigned long int i;
+
+	if (s1 == 0 || s2 == 0)
+		return (NULL);
+	ptr = malloc(strlen(s1) + strlen(s2) + 1);
+	for (i = 0 ; i < strlen(s1) + strlen(s2) + 1 ; i++)
+	{
+		if (i < strlen(s1))
+			ptr[i] = s1[i];
+		if (i > strlen(s1) && i < strlen(s2))
+			ptr[i] = s2[i];
+	}
+return (ptr);
+}
