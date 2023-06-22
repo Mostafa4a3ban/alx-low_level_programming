a2b19e2b03fcfb576de60cf4e3c97ceb7b0444a3
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-22 05:24:20 +0300

    great

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -21,16 +21,16 @@ char *str_concat(char *s1, char *s2)
 		*s2 = '\0';
 	if (s1 == 0 && s2 == 0)
 		return (NULL);
-	ptr = malloc(strlen(s1) + strlen(s2) + 2);
-	for (i = 0 ; i < strlen(s1) + strlen(s2) + 2 ; i++)
+	ptr = malloc(strlen(s1) + strlen(s2) + 1);
+	for (i = 0 ; i < (strlen(s1) + strlen(s2) + 1); i++)
 	{
 		if (i < strlen(s1) && s1 != 0)
 			ptr[i] = s1[i];
 		if (i == strlen(s1))
 			ptr[i] = ' ';
-		if (i > strlen(s1) && i < (strlen(s1) + strlen(s2) + 1) && s2 != 0)
+		if (i > strlen(s1) && i < (strlen(s1) + strlen(s2)) && s2 != 0)
 			ptr[i] = s2[i];
-		if (i > (strlen(s2) + strlen(s1) + 1))
+		if (i == strlen(s2) + strlen(s1))
 			ptr[i] = '\0';
 	}
 return (ptr);
