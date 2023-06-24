c195da3a7daf8f7c6e1d5e16eabd4ee6adfdf09c
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-24 14:59:49 +0300

    done

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 /**
  * free_grid - to free the grid after finishing it
  * @grid: pointer to pointer
- *@height: the height.
+ * @height: the height.
  */
 void free_grid(int **grid, int height)
 {
