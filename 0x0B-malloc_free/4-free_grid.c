cbacea98a08718369d5ac2ac9ac3c91e2df856a5
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-24 14:56:07 +0300

    wow!

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,17 +3,18 @@
 #include <stdlib.h>
 /**
  * free_grid - to free the grid after finishing it
- * @grid: pointer to pointer 
+ * @grid: pointer to pointer
  *@height: the height.
  */
 void free_grid(int **grid, int height)
 {
 	int i;
-	
+
 	i = 0;
 	while (i < height)
 	{
 		free(grid[i]);
+		free(grid);
 		i++;
 	}
 }
