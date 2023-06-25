bd106811354ab3e1d27d5b77eeb85c62b8e7b832
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-06-25 17:08:50 +0300

    it's ok

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,4 +7,6 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 int _putchar(char c);
+char **strtow(char *str);
+int count_word(char *s);
 #endif
