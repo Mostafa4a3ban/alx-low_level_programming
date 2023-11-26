2fe27d07501b6da8c7e671c7b0feb254d137f925
Mostafa4a3ban <mostafashapan4@gmail.com>
2023-11-26 22:40:35 +0200

    The bit maniplation 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,37 @@
-#include <stdio.h>
-#include <math.h>
+#include "main.h"
+
 /**
- ** mbinary_to_uint - check the code
- ***
- *** Return: Always 0, or conversion.
- **
- **/
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int conversion = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
-	{
+	if (!b)
 		return (0);
-	}
-	else
+
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			conversion = conversion * 2 + (b[i] - '0');
-		}
-		else
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-}
-	return (conversion);
+
+	return (ui);
 }
