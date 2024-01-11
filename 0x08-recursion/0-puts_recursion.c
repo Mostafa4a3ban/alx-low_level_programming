791670ebe34d8bdbbc76d79bbd92687f7b4182f4
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-11 02:36:22 +0200

    printing a string recursion way

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *_puts_recursion - print a string recursion way
+ *@s: pointer to character. 
+ *Return: No return
+ *
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if(*s != '\0')
+	{
+		_puts_recursion(s+1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
