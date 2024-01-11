08b3f1d739cecee2eec217456e6f0d9b5e90454d
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-11 02:59:22 +0200

    Recursive string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  *_puts_recursion - print a string recursion way
- *@s: pointer to character. 
+ *@s: pointer to character.
  *Return: No return
  *
  */
 void _puts_recursion(char *s)
 {
 	_putchar(*s);
-	if(*s != '\0')
+	if (*s != '\0')
 	{
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
