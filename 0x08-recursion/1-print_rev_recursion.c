c6f730f0cddf7b15835ba344632b28ab8c8d9887
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-13 01:27:01 +0200

    Edited small things

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *void _print_rev_recursion - printing reverse array
+ *_print_rev_recursion - printing reverse array
  *
  *@s: pointer to a string
  * Return: No return
  */
 void _print_rev_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
 }
