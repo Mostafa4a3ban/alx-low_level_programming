19c334f7df590c930b3d63c5323603a3375258d5
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-14 11:37:40 +0200

    Power x is done!

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _pow_recursion - therecursion for calc the power
+ *
+ *@y: first argueement
+ *@x: secon arguement
+ *Return: -1 if y lower than 0
+ */
+int _pow_recursion(int x, int y)
+{
+	int result = 1;
+
+	if (y > 0)
+	{
+		result = result* x * _pow_recursion(x, y-1);
+	}
+	else if (y == 0)
+	{
+		return 1;
+	}
+	else if(y < 0)
+	{
+		return -1;
+	}
+
+	return result;
+}
