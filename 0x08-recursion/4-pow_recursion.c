a708fbd212f6cd9d04f002eba2431006d04a2cea
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-14 11:40:24 +0200

    Betty code edit

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,16 +12,16 @@ int _pow_recursion(int x, int y)
 
 	if (y > 0)
 	{
-		result = result* x * _pow_recursion(x, y-1);
+		result = result * x * _pow_recursion(x, y - 1);
 	}
 	else if (y == 0)
 	{
-		return 1;
+		return (1);
 	}
-	else if(y < 0)
+	else if (y < 0)
 	{
-		return -1;
+		return (-1);
 	}
 
-	return result;
+	return (result);
 }
