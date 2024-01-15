68bab157b9aeba8490890606a71f9033ed85a2a4
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-16 00:12:12 +0200

    Count of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - the main function
+ *@argc: the number of arguments
+ *@argv: the arrays of strings
+ *Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
