0b8ef5282c9a848e66eccb0043b230321a4049c4
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-16 01:05:54 +0200

    Add with some conditions

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,10 +12,18 @@ int main(int argc, char *argv[])
 
 	if (argc < 3)
 	{
-		printf("Error\n");
+		printf("0\n");
 	}
 	else
 	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] < 48 || *argv[i] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		for (i = 1; i < argc; i++)
 		{
 			result += atoi(argv[i]);
