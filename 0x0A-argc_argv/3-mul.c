cb211748b873771b4bef5e7c9f17f527dcb0928c
Mostafa4a3ban <mostafashapan4@gmail.com>
2024-01-16 00:53:12 +0200

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - the main function
+ *@argc: the first integer
+ *@argv: the array of strings
+ *Return: Error or 0.
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, i;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
